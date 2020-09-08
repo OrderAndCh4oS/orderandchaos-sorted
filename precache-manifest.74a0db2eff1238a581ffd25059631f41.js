89efe2b27c128bdf7e593d2a61320bb664ddbce6
Sarcoma <sarcoma@live.co.uk>
2020-09-08 22:07:39 +0100

    Updates

---
+++ precache-manifest.74a0db2eff1238a581ffd25059631f41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c761ec9cba000bbc3b78c795c81c501",
+    "url": "./index.html"
+  },
+  {
+    "revision": "40684ec0af0d387fbba7",
+    "url": "./static/css/2.fc3c3bbf.chunk.css"
+  },
+  {
+    "revision": "40684ec0af0d387fbba7",
+    "url": "./static/js/2.08b7fffa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.08b7fffa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "620a878b95518f8407ee",
+    "url": "./static/js/main.f924ad13.chunk.js"
+  },
+  {
+    "revision": "d2e4d7fb70f78e3c2c3d",
+    "url": "./static/js/runtime-main.cfa3fac7.js"
+  }
+]);
