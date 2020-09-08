5b6eebd6579248ef56b46b094dbde64d175bbec7
Sarcoma <sarcoma@live.co.uk>
2020-09-08 23:34:34 +0100

    tweak naming

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,20 @@ export type ISortColumns<T> = {
 };
 
 const sortOnColumn = <T>(name: keyof T, isAsc: boolean = true) => {
-    return (a: T, b: T) => {
+    return (objA: T, objB: T) => {
         let result;
-        const aValue = a[name];
-        const bValue = b[name];
-        if (typeof aValue === 'string' && typeof bValue === 'string') {
-            var nameA = aValue.toLowerCase();
-            var nameB = bValue.toLowerCase();
-            result = nameA < nameB ? -1 : nameA > nameB ? 1 : 0;
-        } else if (typeof aValue === 'number' && typeof bValue === 'number') {
-            result = aValue < bValue ? -1 : aValue > bValue ? 1 : 0;
+        const a = objA[name];
+        const b = objB[name];
+        if (typeof a === 'string' && typeof b === 'string') {
+            const strA = a.toLowerCase();
+            const strB = b.toLowerCase();
+            result = strA < strB ? -1 : strA > strB ? 1 : 0;
+        } else if (typeof a === 'number' && typeof b === 'number') {
+            result = a < b ? -1 : a > b ? 1 : 0;
         } else {
-            const aStr = String(aValue).toLowerCase();
-            const bStr = String(bValue).toLowerCase();
-            result = aStr < bStr ? -1 : aStr > bStr ? 1 : 0;
+            const aAsStr = String(a).toLowerCase();
+            const bAsStr = String(b).toLowerCase();
+            result = aAsStr < bAsStr ? -1 : aAsStr > bAsStr ? 1 : 0;
         }
 
         return isAsc ? result : -result;
